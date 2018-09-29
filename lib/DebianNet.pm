guillemj/update-inetd

commit 75d5623907045eacf7e732797583bd1db7b234aa

Do not import Debconf::Client::ConfModule globally
Only call import in case we are going to use the module, so that we
avoid annoying warnings when not running the script as root, as part
of the test suite.

lib/DebianNet.pm
@@ -46,7 +46,7 @@ package DebianNet;
 
 require 5.6.1;
 
-use Debconf::Client::ConfModule ':all';
+use Debconf::Client::ConfModule ();
 
 BEGIN {
     eval 'use File::Temp qw/ tempfile /';
@@ -147,6 +147,13 @@ our $called_wakeup_inetd = 0;
 
 =over 4
 
+=cut
+
+sub _debconf_init
+{
+    Debconf::Client::ConfModule->import(':all');
+}
+
 =item $rc = DebianNet::add_service($newentry, $group)
 
 Add $newentry to the group $group of the F</etc/inetd.conf> file. If the
@@ -192,6 +199,8 @@ sub add_service {
             &enable_service($sservice);
         } else {
             if (grep(m/^$sservice\s+/,@inetd)) {
+                _debconf_init();
+
                 if (grep(m/^$sservice\s+/,@inetd) > 1) {
                     set("update-inetd/ask-several-entries", "true");
                     fset("update-inetd/ask-several-entries", "seen", "false");
@@ -300,6 +309,8 @@ sub remove_service {
 
     if (((&scan_entries("$service", $pattern) > 1) or (&scan_entries("$sep$service", $pattern) > 1))
         and (not defined($multi))) {
+        _debconf_init();
+
         set("update-inetd/ask-remove-entries", "false");
         fset("update-inetd/ask-remove-entries", "seen", "false");
             settitle("update-inetd/title");
@@ -360,6 +371,8 @@ sub disable_service {
     my $nlines_disabled = 0;
 
     if ((&scan_entries("$service", $pattern) > 1) and (not defined($multi))) {
+        _debconf_init();
+
         set("update-inetd/ask-disable-entries", "false");
         fset("update-inetd/ask-disable-entries", "seen", "false");
             settitle("update-inetd/title");

commit cff327d75d80332e06dafda7b8bd4f6854992bd8

Preserve the original @ARGV
Pass it to the Debconf module so that when that tries to reexecute us
we get the correct arguments.

Fixes: commit 75d5623907045eacf7e732797583bd1db7b234aa

lib/DebianNet.pm
@@ -148,6 +148,7 @@ our $VERBOSE;
 =cut
 
 our $INETD_WAKEUP_CALLED = 0;
+our @DEBCONF_ARGV = ();
 
 # Backwards compatibility aliases.
 ## no critic (Variables::ProhibitPackageVars)
@@ -173,6 +174,7 @@ our $called_wakeup_inetd;
 
 sub _debconf_init
 {
+    local @ARGV = @DEBCONF_ARGV;
     Debconf::Client::ConfModule->import(':all');
 }
 

update-inetd
@@ -25,6 +25,11 @@ use warnings;
 
 use DebianNet ();
 
+# We need to preserve the original @ARGV, otherwise when the Debconf module
+# tries to reexecute us after it has started the backend it would miss
+# arguments.
+@DebianNet::DEBCONF_ARGV = @ARGV;
+
 $| = 1;
 
 $0 =~ s{.*/}{};